e6daef9c0d93ce3360a500896850b3548756204e
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-08-08 11:24:45 -0300

    arrumando algumas coisas

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 const botaoTrocarImg = document.getElementById('trocar-img')
 
 function trocarImg(){
-    const img = document.getElementById('fundo').value
-    document.documentElement.style.setProperty('--img-background', fundo)
+    const img = document.getElementById('img').value
+    document.documentElement.style.setProperty( '--img-background', fundo)
+    console.log(img)
 }
 
 botaoTrocarImg.addEventListener('click', trocarImg)
@@ -14,27 +15,22 @@ botaoTrocarImg.addEventListener('click', trocarImg)
 
 
 //  ESTÁ TROCANDO PORÉM ELE ATIVA DIRETO, O BOTÃO NÃO ESTÁ FUNCIONANDO
-// const botaoAuto = document.getElementById('trocar-auto')
-// const imgs = [
-//     ['img/doflamingo.webp'],
-//     ['img/kid.webp'],
-//     ['img/law.webp'],
-//     ['img/lucci.jpeg'],
-//     ['img/luffy.webp']
-// ]
+const botaoAuto = document.getElementById('trocar-auto')
+const imgs = [
+    ['img/doflamingo.webp'],
+    ['img/kid.webp'],
+    ['img/law.webp'],
+    ['img/lucci.jpeg'],
+    ['img/luffy.webp']
+]
 
-// let imgAtual = 0 /* váriavel da imagem atual */
-// const fundo = document.getElementById('carrossel') /* constante que guarda as imagens */
+let imgAtual = 0 /* váriavel da imagem atual */
+const fundo = document.getElementById('carrossel') /* constante que guarda as imagens */
 
-// function trocarAuto(){
-//     const imgs = document.getElementById('fundo').value
-//     document.documentElement.style.setProperty('--img-background', fundo)
-// }
+function atualizarImagem(){
+    carrossel.style.backgroundImage = `url(${imgs[imgAtual][0]})` /* pega uma imagem da constante imgs */
+    imgAtual = (imgAtual + 1) % imgs.length /* adiciona mais 1 ao imgAtual, fazendo mudar para a proxima imagem */
+}                                           /* o operador % faz voltar para o inicio toda vez que acaba */
 
-// function atualizarImagem(){
-//     carrossel.style.backgroundImage = `url(${imgs[imgAtual][0]})` /* pega uma imagem da constante imgs */
-//     imgAtual = (imgAtual + 1) % imgs.length /* adiciona mais 1 ao imgAtual, fazendo mudar para a proxima imagem */
-// }                                           /* o operador % faz voltar para o inicio toda vez que acaba */
 
-// setInterval(atualizarImagem, 3000)
-// botaoAuto.addEventListener('click', trocarAuto)
+botaoAuto.addEventListener('click', setInterval(atualizarImagem, 3000))
