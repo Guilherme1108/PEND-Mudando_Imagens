89eec6e69a2b997edad1750dfd479c62d1e6314e
Guilherme1108 <guilherme.moreira1108@gmail.com>
2025-08-10 18:29:33 -0300

    os 2 botões estão funcionando

--- app.js
+++ app.js
@@ -23,23 +23,37 @@ function trocarImg() {
 botaoTrocarImg.addEventListener('click', trocarImg);
 
 
-//  ESTÁ TROCANDO PORÉM ELE ATIVA DIRETO, O BOTÃO NÃO ESTÁ FUNCIONANDO
-const botaoAuto = document.getElementById('trocar-auto')
+
+
+const botaoAuto = document.getElementById('botaoAuto');
+
+//array de imagens
 const imgs = [
     ['img/doflamingo.webp'],
     ['img/kid.webp'],
     ['img/law.webp'],
-    ['img/lucci.jpeg'],
+    ['img/lucci.webp'],
     ['img/luffy.webp']
-]
+];
 
-let imgAtual = 0 /* váriavel da imagem atual */
-const fundo = document.getElementById('carrossel') /* constante que guarda as imagens */
-
-function atualizarImagem(){
-    carrossel.style.backgroundImage = `url(${imgs[imgAtual][0]})` /* pega uma imagem da constante imgs */
-    imgAtual = (imgAtual + 1) % imgs.length /* adiciona mais 1 ao imgAtual, fazendo mudar para a proxima imagem */
-}                                           /* o operador % faz voltar para o inicio toda vez que acaba */
+let imgAtual = 0; // variável da imagem atual
 
 
-botaoAuto.addEventListener('click', setInterval(atualizarImagem, 3000))
+function atualizarImagem() { //função que faz mudar a imagem
+    let caminho = imgs[imgAtual][0];/* pega uma imagem da constante img */
+    document.documentElement.style.setProperty('--img-background', `url(${caminho})`); //coloca o caminho da imagem no root
+    imgAtual = (imgAtual + 1) % imgs.length; /* adiciona mais 1 ao imgAtual, fazendo mudar para a proxima imagem */
+}                                            /* o operador % faz voltar para o inicio toda vez que acaba */
+
+let intervalo = null; // para controlar o setInterval
+
+// inicia a troca quando clicar no botão
+botaoAuto.addEventListener('click', function () {
+    if (intervalo) { //verifica se existe algum intervalo
+        clearInterval(intervalo); //se existir ele vai limparo intervalo
+        intervalo = null; //e definir que não existe mais
+    } else { //se não existir intervalo ele recebe false e entra no else
+        intervalo = setInterval(atualizarImagem, 2000); // e da um intervalo de 
+    }
+});
+
